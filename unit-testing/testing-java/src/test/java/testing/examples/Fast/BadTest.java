7ec753e7af6f2664d18cc1b1a1d95a3f1f9172d5
Janos Gyerik <janos@kronos>
2015-11-19 00:28:14 +0100

    added bad example of slow tests

---
+++ unit-testing/testing-java/src/test/java/testing/examples/Fast/BadTest.java
@@ -0,0 +1,32 @@
+package testing.examples.Fast;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import testing.common.ExcelSheetUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public class BadTest {
+
+    private int titleToNumber(String title) {
+        return ExcelSheetUtils.titleToNumber(title);
+    }
+
+    // BAD: test cases should be fast
+    //
+    // Paranoia should have its limits. There's no need to check the
+    // full range of possible inputs. Cover the corner cases,
+    // and pick some interesting cases.
+
+    @Ignore
+    @Test
+    public void test_a_to_z() {
+        for (int z = 0; z < 1000000; ++z) {
+            for (int i = 'A'; i < 'Z'; ++i) {
+                for (int j = 'A'; j < 'Z'; ++j) {
+                    assertEquals(1 + i - 'A' + (1 + j - 'A') * 26, titleToNumber("" + (char) j + (char) i));
+                }
+            }
+        }
+    }
+}
---
+++ unit-testing/testing-java/src/test/java/testing/examples/Fast/GoodTest.java
@@ -0,0 +1,36 @@
+package testing.examples.Fast;
+
+import org.junit.Test;
+import testing.common.ExcelSheetUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public class GoodTest {
+
+    private int titleToNumber(String title) {
+        return ExcelSheetUtils.titleToNumber(title);
+    }
+
+    // GOOD: these test cases are simple and fast
+
+    @Test
+    public void test_a() {
+        assertEquals(1, titleToNumber("A"));
+    }
+
+    @Test
+    public void test_z() {
+        assertEquals(26, titleToNumber("Z"));
+    }
+
+    @Test
+    public void test_aa() {
+        assertEquals(27, titleToNumber("AA"));
+    }
+
+    @Test
+    public void test_bb() {
+        assertEquals(54, titleToNumber("BB"));
+    }
+
+}

2a3bc13097edec8ecb938f6a345f112b342e84ca
Janos Gyerik <janos@kronos>
2015-11-19 00:42:16 +0100

    added placeholders for unimplemented examples

---
+++ unit-testing/testing-java/src/test/java/testing/examples/Simple/GoodTest.java
@@ -0,0 +1,36 @@
+package testing.examples.EasyToRead;
+
+import org.junit.Test;
+import testing.common.ExcelSheetUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public class GoodTest {
+
+    private int titleToNumber(String title) {
+        return ExcelSheetUtils.titleToNumber(title);
+    }
+
+    // GOOD: all test cases are nice and simple, trivially easy to understand
+
+    @Test
+    public void test_a() {
+        assertEquals(1, titleToNumber("A"));
+    }
+
+    @Test
+    public void test_z() {
+        assertEquals(26, titleToNumber("Z"));
+    }
+
+    @Test
+    public void test_aa() {
+        assertEquals(27, titleToNumber("AA"));
+    }
+
+    @Test
+    public void test_bb() {
+        assertEquals(54, titleToNumber("BB"));
+    }
+
+}

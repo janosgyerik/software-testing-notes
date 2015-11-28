b0b72e398ad7111d5dae2baef771c831219abd5a
Janos Gyerik <janos@kronos>
2015-11-28 23:57:29 +0100

    improved local data good bad examples

--- unit-testing/testing-java/src/test/java/testing/examples/NoLocalData/BadTest.java
+++ unit-testing/testing-java/src/test/java/testing/examples/NoLocalData/BadTest.java
@@ -14,16 +14,22 @@ public class BadTest {
         return ExcelSheetUtils.titleToNumber(title);
     }
 
-    // BAD: test cases should use data from the local filesystem.
+    // BAD: test cases should NOT use data from the local filesystem.
+    //
     // Test cases that depend on local data are effectively broken.
-    // If you want to read data from files, put them in src/test/resources
+    // If you want to read data from files, put them in src/test/resources,
+    // inside project, under version control, together with the test.
 
-    private static final String TEST_DATA_SHEET_CSV = "c:/tmp/test-data-sheet.csv";
+    private static final String TEST_DATA_SHEET_CSV = "c:/tmp/data.csv";
+
+    private Scanner getTestDataScanner() {
+        return new Scanner(this.getClass().getClassLoader().getResourceAsStream(TEST_DATA_SHEET_CSV));
+    }
 
     @Ignore
     @Test
     public void test_examples_in_file() {
-        Scanner scanner = new Scanner(TEST_DATA_SHEET_CSV);
+        Scanner scanner = getTestDataScanner();
         while (scanner.hasNextLine()) {
             String[] parts = scanner.nextLine().split(",");
             int expected = Integer.parseInt(parts[0]);

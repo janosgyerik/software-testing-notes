7cbec57207f69888f9881c4b526977aeb4a2d7b9
Janos Gyerik <janos@kronos>
2015-12-01 20:29:58 +0100

    made test_a_bunch_of_things more horrible

--- unit-testing/testing-java/src/test/java/testing/examples/OneTestOneMethod/BadTest.java
+++ unit-testing/testing-java/src/test/java/testing/examples/OneTestOneMethod/BadTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import testing.common.ExcelSheetUtils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static testing.common.ExcelSheetUtils.titleToNumber;
 
 public class BadTest {
@@ -15,9 +16,26 @@ public class BadTest {
 
     @Test
     public void test_a_bunch_of_things() {
+        try {
+            titleToNumber("");
+            fail();
+        } catch (IllegalArgumentException e) {
+            // good, as expected!
+        }
+
         assertEquals(1, titleToNumber("A"));
         assertEquals(26, titleToNumber("Z"));
         assertEquals(27, titleToNumber("AA"));
         assertEquals(54, titleToNumber("BB"));
+
+        for (int j = 'A'; j < 'Z'; ++j) {
+            assertEquals(1 + 'A' - 'A' + (1 + j - 'A') * 26, titleToNumber("" + (char) j + 'A'));
+        }
+        for (int j = 'A'; j < 'Z'; ++j) {
+            assertEquals(1 + 'B' - 'A' + (1 + j - 'A') * 26, titleToNumber("" + (char) j + 'B'));
+        }
+        for (int j = 'A'; j < 'Z'; ++j) {
+            assertEquals(1 + 'C' - 'A' + (1 + j - 'A') * 26, titleToNumber("" + (char) j + 'C'));
+        }
     }
 }
